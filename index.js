4fdd64f4b21d2464288b8333177ad48d0098a013
andreaspencer <andreaaddawn@gmail.com>
2021-10-16 09:59:13 -0700

    file setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const fs = require("fs");
